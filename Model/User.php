4f6d3ad6a73cca719dfd9612da3f66843925af51
Carlos Mendoza <inhack20@gmail.com>
2014-12-17 18:50:27 -0430

    Agregando metodos al usuario

---
+++ Model/User.php
@@ -0,0 +1,32 @@
+<?php
+
+/*
+ * This file is part of the TecnoCreaciones package.
+ * 
+ * (c) www.tecnocreaciones.com
+ * 
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace BtoB\SocialNetwork\CoreBundle\Model;
+
+use FOS\UserBundle\Model\User as BaseUser;
+
+/**
+ * Description of User
+ *
+ * @author Carlos Mendoza <inhack20@tecnocreaciones.com>
+ */
+abstract class User 
+{
+    protected $percentageComplete;
+    
+    function isValidProfile()
+    {
+        if($this->percentageComplete >= 100){
+            return true;
+        }
+        return false;
+    }
+}
